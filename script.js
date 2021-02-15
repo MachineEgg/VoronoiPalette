ae62755896eb2d9ee5a56e00634641208f5d6657
MachineEgg <38972066+MachineEgg@users.noreply.github.com>
2021-02-14 21:17:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,38 @@ const activeSaturationSlider = document.getElementById('activeSaturationSlider')
 const activeSaturationField = document.getElementById('activeSaturationField');
 const activeLightnessSlider = document.getElementById('activeLightnessSlider');
 const activeLightnessField = document.getElementById('activeLightnessField');
+const deleteButton = document.getElementById('deleteButton');
 
+
+//color table reference
+const colorTable = document.getElementById('colorTable');
+const activeHex = document.getElementById('activeHex');
 
 let colors = [];
 let saturation = 60;
 
 let activeColor = 0;
 
+function rgb2hex(r, g, b){
+	r = r.toString(16);
+	g = g.toString(16);
+	b = b.toString(16);
+
+	if (r.length == 1)
+		r = "0" + r;
+	if (g.length == 1)
+		g = "0" + g;
+	if (b.length == 1)
+		b = "0" + b;
+
+	return "#" + r + g + b;
+}
+
 function hsl2rgb (h,s,l) {
   
   let c = (1 - Math.abs(2 * l - 1)) * s,
-      x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-      m = l - c/2,
+      x = c * (1 - Math.abs((h / 60.0) % 2 - 1)),
+      m = l - c/2.0,
       r = 0,
       g = 0,
       b = 0;
@@ -48,6 +68,8 @@ function hsl2rgb (h,s,l) {
   return [r,g,b];
 }
 
+
+
 function drawRamp()
 {
 	let image = ctx.createImageData(360,360);
@@ -56,7 +78,7 @@ function drawRamp()
 
 	for (let x = 0; x < 360; x++) {
 		for( let y = 0; y < 360; y++) {
-			let [red, green, blue] = hsl2rgb(x,saturation/100,1-y/360);
+			let [red, green, blue] = hsl2rgb(x,saturation/100,y/360);
 			
 			let index = (x + (y*360)) * 4;
 			data[index] = red;
@@ -73,7 +95,7 @@ function drawRamp()
 
 		let posX = colors[i]-size;
 		let posY = colors[i+1]-size;
-		let [red, green, blue] = hsl2rgb(colors[i],colors[i+2]/100,1-colors[i+1]/360);
+		let [red, green, blue] = hsl2rgb(colors[i],colors[i+2]/100,colors[i+1]/360);
 		
 		for (let x = 0; x < 1+size*2; x++) {
 			for( let y = 0; y < 1+size*2; y++) {
@@ -124,14 +146,14 @@ function drawVoronoi()
 
 			for(i=0; i<colors.length; i+=3){
 
-				newDist = Math.sqrt(Math.pow(Math.min(Math.abs(x - colors[i]),Math.abs(x-colors[i]-360))*0.5,2) + Math.pow(y - colors[i+1],2));
+				newDist = Math.sqrt(Math.pow(Math.min(x-colors[i]+360,Math.min(Math.abs(x - colors[i]),Math.abs(x-colors[i]-360))),2) + Math.pow(y - colors[i+1],2));
 				if (newDist<dist) {
 					target=i;
 					dist=newDist;
 				}
 			}
 
-			let [red, green, blue] = hsl2rgb(colors[target],colors[target+2]/100,1-colors[target+1]/360);
+			let [red, green, blue] = hsl2rgb(colors[target],colors[target+2]/100,colors[target+1]/360);
 			
 			let index = (x + (y*360)) * 4;
 			data[index] = red;
@@ -143,7 +165,53 @@ function drawVoronoi()
 	ctx.putImageData(image,360,0)
 }
 
+function updateTable()
+{
+	let counter = 0;
+	let newTable = "<table><colgroup><col span=\"10\" width=\"72\"></colgroup><tr>";
+	
+	for(i=0; i<colors.length; i+=3){
+		let [red,green,blue] = hsl2rgb(colors[i],colors[i+2]/100,colors[i+1]/360);
+		let colorHex = rgb2hex(red, green, blue);
+		newTable+="<td style=\"background-color:" + colorHex + ";color:";
+		
+		if (colors[i+1]>100){ 
+			newTable += "#000000\">" + colorHex + "</td>";
+		} else {
+			newTable += "#FFFFFF\">" + colorHex + "</td>";
+		}
 
+		
+		counter++;
+		if (counter>=10){
+			newTable += "</tr><tr>";
+			counter = 0;
+		}
+		
+	}
+
+
+	newTable += "</tr></table>";
+
+	colorTable.innerHTML = newTable;
+	
+	newTable="";
+	if (colors.length>0){
+	let [red,green,blue] = hsl2rgb(colors[activeColor],colors[activeColor+2]/100,colors[activeColor+1]/360);
+	let colorHex = rgb2hex(red, green, blue);
+	
+	newTable = "<div style=\"background-color:" + colorHex  + ";color:";
+
+	if (colors[activeColor+1]>100){ 
+		newTable += "#000000\">" + colorHex + "</div>";
+	} else {
+		newTable += "#FFFFFF\">" + colorHex + "</div>";
+	}
+	}
+	activeHex.innerHTML = newTable;
+	
+
+}
 
 
 
@@ -174,10 +242,10 @@ function updateActive()
 	if(activeColor<colors.length){
 		activeHueField.value = colors[activeColor];
 		activeHueSlider.value = colors[activeColor];
-		activeSaturationField.value = colors[activeColor];
-		activeSaturationSlider.value = colors[activeColor];
-		activeLightnessField.value = colors[activeColor];
-		activeLightnessSlider.value = colors[activeColor];
+		activeSaturationField.value = colors[activeColor+2];
+		activeSaturationSlider.value = colors[activeColor+2];
+		activeLightnessField.value = Math.floor(colors[activeColor+1]/3.6);
+		activeLightnessSlider.value = Math.floor(colors[activeColor+1]/3.6);
 	}
 }
 
@@ -188,15 +256,39 @@ activeHueSlider.oninput = function(){
 		activeHueField.value=this.value;
 		drawRamp();
 		drawVoronoi();
+		updateTable();
+
 	}
 }
 activeHueField.oninput = function(){
 	if(activeColor<colors.length){
-		this.value = Math.floor(Math.max(Math.min(this.value,360),0));
+		this.value = Math.floor(Math.max(Math.min(this.value,359),0));
 		colors[activeColor] = this.value;
 		activeHueSlider.value=this.value;
 		drawRamp();
 		drawVoronoi();
+		updateTable();
+
+	}
+}
+activeSaturationSlider.oninput = function(){
+	if(activeColor<colors.length){
+		colors[activeColor+2] = this.value;
+		activeSaturationField.value=this.value;
+		drawRamp();
+		drawVoronoi();
+		updateTable();
+
+	}
+}
+activeSaturationField.oninput = function(){
+	if(activeColor<colors.length){
+		colors[activeColor+2] = this.value;
+		activeSaturationSlider.value=this.value;
+		drawRamp();
+		drawVoronoi();
+		updateTable();
+
 	}
 }
 activeLightnessSlider.oninput = function(){
@@ -205,6 +297,8 @@ activeLightnessSlider.oninput = function(){
 		activeLightnessField.value=this.value;
 		drawRamp();
 		drawVoronoi();
+		updateTable();
+
 	}
 }
 activeLightnessField.oninput = function(){
@@ -214,40 +308,133 @@ activeLightnessField.oninput = function(){
 		activeLightnessSlider.value=this.value;
 		drawRamp();
 		drawVoronoi();
+		updateTable();
 	}
 }
 
-//add/select color
-canvas.addEventListener('click', function(event) {
-	let rect = canvas.getBoundingClientRect();
-	let x = Math.floor(event.clientX - rect.left);
-	let y = Math.floor(event.clientY - rect.top);
-	if (x<360) {
-		activeColor=colors.length;
-		colors.push(x);
-		colors.push(y);
-		colors.push(saturation);
-	} else if (x<720 && colors.length > 0) {
+
+//delete
+deleteButton.onclick = function(){
+	if(activeColor<colors.length){
+		colors.splice(activeColor,3);
+
+		if(activeColor=>colors.length){
+			activeColor = colors.length-3;
+			if(activeColor<0){
+				activeColor = 0;
+			}
+		}
+
+		drawVoronoi();
+		drawRamp();
+		updateActive();
+		updateTable();
+
+	}
+}
+
+//add/move/select color
+function addColor(x,y){
+
+}
+
+function moveColor(x,y){
+
+}
+
+
+let isMoving = false;
+
+
+// Add the event listeners for mousedown, mousemove, and mouseup
+canvas.addEventListener('mousedown', e => {
+  	let rect = canvas.getBoundingClientRect();
+	let x = Math.floor(e.clientX - rect.left);
+	let y = Math.floor(e.clientY - rect.top);
+	isDrawing = true;
+
+	if (x<360) {//move or add
+		let selectedColor = -1;
+		
+		if (colors.length>0){
+			for(i=0; i<colors.length; i+=3){
+				let size = 3;
+				if (activeColor == i) size = 5;
+	
+				let posX = colors[i];
+				let posY = colors[i+1];
+		
+				if(x>=posX-size && y>=posY-size && x<=posX+size && y<=posY+size){
+					selectedColor=i;
+				}
+			}		
+		}
+ 		
+		if(selectedColor>=0){
+			activeColor = selectedColor;
+		} else {
+			activeColor=colors.length;
+			colors.push(x);
+			colors.push(y);
+			colors.push(saturation);
+		}
+		isMoving = true;
+	} else if (x<720 && colors.length > 0) {//select
 		x-=360;
 		let dist = 1024;
 		let newDist = 0;
 		let target=0
 			
 		for(i=0; i<colors.length; i+=3){
-			newDist = Math.sqrt(Math.pow(Math.min(Math.abs(x - colors[i]),Math.abs(x-colors[i]-360))*0.5,2) + Math.pow(y - colors[i+1],2));
+			newDist = Math.sqrt(Math.pow(Math.min(x-colors[i]+360,Math.min(Math.abs(x - colors[i]),Math.abs(x-colors[i]-360))),2) + Math.pow(y - colors[i+1],2));
 			if (newDist<dist) {
 				target=i;
 				dist=newDist;
 			}
 		}
 		activeColor = target;
-
 	}
-	
 	drawVoronoi();
 	drawRamp();
 	updateActive();
-}, false);
+	updateTable();
+});
+
+canvas.addEventListener('mousemove', e => {
+	if (isMoving == true) {
+		let rect = canvas.getBoundingClientRect();
+		let x = Math.floor(e.clientX - rect.left);
+		let y = Math.floor(e.clientY - rect.top);
+		
+		if (x>=0&&y>=0&&x<360&&y<360) {
+			colors[activeColor]=x;
+			colors[activeColor+1]=y;
+			drawVoronoi();
+ 			drawRamp();
+			updateActive();
+			updateTable();
+		}
+	}
+});
+
+window.addEventListener('mouseup', e => {
+	if (isMoving == true) {
+		let rect = canvas.getBoundingClientRect();
+		let x = Math.floor(e.clientX - rect.left);
+		let y = Math.floor(e.clientY - rect.top);
+
+		if (x>=0&&y>=0&&x<360&&y<360) {
+			colors[activeColor]=x;
+			colors[activeColor+1]=y;
+			drawVoronoi();
+ 			drawRamp();
+			updateActive();
+			updateTable();
+		}
+	}
+	isMoving = false;
+});
+
 
 
 
